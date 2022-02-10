b2626d639dcd8ebdcce7855c8c1bd082d3296166
Sejmou <samo.kolter@gmail.com>
2022-02-10 18:15:13 +0100

    add image + preview for message, basic styling

--- app/src/main/java/at/ac/tuwien/composeintro/MainActivity.kt
+++ app/src/main/java/at/ac/tuwien/composeintro/MainActivity.kt
@@ -3,10 +3,17 @@ package at.ac.tuwien.composeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import at.ac.tuwien.composeintro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +29,27 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {// need column, else texts rendered over each other
-        Text(text = msg.author)
-        Text(text = msg.body)
+    // Add padding around message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as circle
+                .clip(CircleShape)
+        )
+
+        // Add horizontal space between image and column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {// need column, else texts rendered over each other
+            Text(text = msg.author)
+            // Add vertical space between author and message text
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
     }
 }
 
